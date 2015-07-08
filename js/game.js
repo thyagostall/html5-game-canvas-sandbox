f20fe349eb7ee5bacf012dc9fc26d1119e815aeb
thyagostall <thstall@gmail.com>
2015-07-08 17:59:41 -0400

    Corrected bug-0.0.2

--- js/game.js
+++ js/game.js
@@ -200,6 +200,11 @@ function Pong() {
         document.addEventListener("keyup", function(evt) {
             delete keystate[evt.keyCode];
         });
+        window.addEventListener("keydown", function(e) {
+            if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+                e.preventDefault();
+            }
+        }, false);
 
         init();
 
